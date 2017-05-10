4eb492db9bcb16c65219d36cbf2657cf449ac5b4
emunicio <emunicio@gmail.com>
2017-05-10 12:22:54 +0200

    Updating SimStats

--- SimEngine/SimStats.py
+++ SimEngine/SimStats.py
@@ -6,6 +6,7 @@
 \author Kazushi Muraoka <k-muraoka@eecs.berkeley.edu>
 \author Nicola Accettura <nicola.accettura@eecs.berkeley.edu>
 \author Xavier Vilajosana <xvilajosana@eecs.berkeley.edu>
+Added changes for 6tisch-sim-extended: Esteban Municio <esteban.municio@uantwerpen.be>
 '''
 
 #============================ logging =========================================
@@ -22,6 +23,7 @@ log.addHandler(NullHandler())
 
 import SimEngine
 import SimSettings
+import json
 
 #============================ defines =========================================
 
@@ -76,33 +78,116 @@ class SimStats(object):
             cb          = self._actionEnd,
         )
         
+	#determine if the experiment has been scheduled   
+ 	self.endScheduled=False       
     
     def destroy(self):
         # destroy my own instance
         self._instance                      = None
         self._init                          = False
-    
+
+
     #======================== private =========================================
     
     def _actionStart(self):
         '''Called once at beginning of the simulation.'''
-	#schedule a pause
         #self.engine.pauseAtAsn(13130)
         pass
-    
+
+    #@profile
     def _actionEndCycle(self):
         '''Called at each end of cycle.'''
         
         cycle = int(self.engine.getAsn()/self.settings.slotframeLength)
-        
-	#emunicio        
-	# start probing at cycle 63 
-        if (self.engine.asn > (63*self.settings.slotframeLength)) and self.engine.initTimeStampTraffic==0:	
-            self.engine.initTimeStampTraffic=self.engine.asn*self.settings.slotDuration
 
-        # stop probing at cycle 96   
-        if (self.engine.asn >= (96*self.settings.slotframeLength)) and self.engine.endTimeStampTraffic==0:
+	#variable end
+	if self.engine.experimentEndTime==(self.settings.numCyclesPerRun):
+       		print('   cycle: {0}/{1}?'.format(cycle,self.settings.numCyclesPerRun))
+	else:
+		print('   cycle: {0}/{1}'.format(cycle,self.engine.experimentEndTime-1))
+	
+	#update rssi values of all nodes at every cycle
+	if self.settings.mobilityModel=='RPGM':
+	    #start moving when the experiment starts
+	    if self.endScheduled:
+	        if not self.engine.allMotesGrouped():
+		    for m in self.engine.motes:
+			m._updateLocation()
+	
+	#update rssi values of all nodes but the root at every cycle
+	if self.settings.mobilityModel=='RWM':
+	    #start moving when the experiment starts
+	    if self.endScheduled:
+		if not self.engine.allMotesGrouped():
+		    for m in self.engine.motes:
+			if m.id!=0:
+			    m._updateLocation()
+
+	#update rssi values of all nodes at every cycle
+	if self.endScheduled:
+	    if self.settings.mobilityModel!='static':	#there are: static | staticRay | staticUNI | RPGM | RWM
+		self.engine.topology.updateTopology() 
+
+		
+	#once the nodes have a parent, secuencially trigger sf0
+	if self.engine.getJoinedNodes()>=(self.settings.numMotes-1):
+	    if self.engine.turn==1:
+		for m in self.engine.motes:
+			if m.id==1:
+			    if m.otfTriggered==False:
+				m.otfTriggered=True
+				m._otf_schedule_housekeeping(firstOtf=True)	 
+			
+
+	if self.engine.getSendingNodes()==self.settings.numMotes-1:
+	    if self.engine.checkConvergence():
+		    if not self.endScheduled:	
+			self.endScheduled=True
+			initcycle=cycle+40+2*self.settings.numMotes/(self.settings.numSHAREDCells*self.settings.numRadios)
+			endcycle=cycle+240+2*self.settings.numMotes/(self.settings.numSHAREDCells*self.settings.numRadios)
+			self.engine.scheduleEndSimAt(initcycle,endcycle)
+
+			#when the experiment starts, save topology
+			self._generateTopologyJsonFile()
+
+			print "Sheduled init experiment at at "+str(initcycle)
+			print "Sheduled end at "+str(endcycle)
+
+
+
+	if not self.endScheduled:
+	    for n in self.engine.motes:
+		if n.id!=0:
+			if self.engine.saturationReached==True:	#saturation has been reached in any of the nodes when starting. Initializating sending packets		    
+			    
+			    if self.engine.simulationForced==False:
+			    	self.engine.simulationForced=True
+			    if n.otfTriggered==False:
+				n.otfTriggered=True
+				n._otf_schedule_housekeeping(firstOtf=True)
+			    if n.sendingPacketsTriggered==False:
+				n.sendingPacketsTriggered=True
+				n._app_schedule_sendSinglePacket(firstPacket=True)
+			    		
+  
+			if cycle>=1700:	#joning process is taking some time. Forcing experiment to start
+			    if self.engine.simulationForced==False:
+			    	self.engine.simulationForced=True
+			    if n.otfTriggered==False:
+				n.otfTriggered=True
+				n._otf_schedule_housekeeping(firstOtf=True)
+			    if n.sendingPacketsTriggered==False:
+				n.sendingPacketsTriggered=True
+				n._app_schedule_sendSinglePacket(firstPacket=True)		
+
+
+
+	if (self.engine.asn > ((self.engine.experimentInitTime-1)*self.settings.slotframeLength)) and self.engine.initTimeStampTraffic==0:
+            self.engine.initTimeStampTraffic=self.engine.asn*self.settings.slotDuration
+         
+        if (self.engine.asn >= ((self.engine.experimentEndTime-1)*self.settings.slotframeLength)) and self.engine.endTimeStampTraffic==0:
             self.engine.endTimeStampTraffic=self.engine.asn*self.settings.slotDuration
+            
             self.engine.timeElapsedFlow=self.engine.endTimeStampTraffic-self.engine.initTimeStampTraffic        
             print "Elapsed time: "+str(self.engine.timeElapsedFlow)        
         
@@ -128,7 +213,7 @@ class SimStats(object):
         
     def _actionEndSecond(self):
         '''Called at each end of cycle.'''
-                
+        
         # schedule next statistics collection
         self.engine.scheduleAtAsn(
             asn         = self.engine.getAsn()+100,
@@ -139,7 +224,13 @@ class SimStats(object):
     
     def _actionEnd(self):
         '''Called once at end of the simulation.'''
-        self._fileWriteTopology()
+	
+	#write topology at the end of the results
+        #self._fileWriteTopology()	# not used to avoid .ods files get too big
+
+	#write summary at the end of the results
+	self._fileWriteSummary()
+	
     
     #=== collecting statistics
     
@@ -155,13 +246,11 @@ class SimStats(object):
                     returnVal[k] += moteStats[k]
         
         return returnVal
-
-    
-        
+           
     def _collectScheduleStats(self):
         
         # compute the number of schedule collisions
-        
+
         # Note that this cannot count past schedule collisions which have been relocated by 6top
         # as this is called at the end of cycle   
         scheduleCollisions = 0
@@ -193,7 +282,7 @@ class SimStats(object):
         collidedTxs = 0
         for links in collidedLinks:
             collidedTxs += len(links)
-              
+        
         # compute the number of effective collided Tx    
         effectiveCollidedTxs = 0
         insufficientLength   = 0 
@@ -242,6 +331,43 @@ class SimStats(object):
         with open(self.settings.getOutputFile(),'a') as f:
             f.write('\n'.join(output))
     
+    def _generateTopologyJsonFile(self):
+
+	data={}
+	
+	filen=self.settings.getOutputFile().split(".")[0]
+
+	filename="topology-"+filen+".json"
+
+	with open(filename, 'w') as outfile:	
+
+	    for mote in self.engine.motes:
+		moteInfo={}
+		neighbors={}
+		moteInfo['x']=mote.x
+		moteInfo['y']=mote.y
+		for m in self.engine.motes:
+		    if m!=mote:
+			neighbors[m.id]=mote.getRSSI(m)
+		moteInfo['neighbors']=neighbors
+		if mote.id==0:
+			moteInfo['parent']="Root"
+		else:
+			moteInfo['parent']=str(mote.preferredParent.id)
+		data[mote.id]=moteInfo
+	    json.dump(data, outfile)	
+
+    def _fileWriteSummary(self):
+	output  = []
+	output += [
+            '#results runNum={0} totalPacketSent {1} totalPacketReceived {2} totalOLGenerated {3} totalThReceived {4} dropsByCollisions {5} totalTX {6} totalRX {7} dropsPropagation {8} joiningTime {9} txcellsTime {10} sendingtime {11} avgVisibleNeigh {12} TRX {13} RDX {14} Locked {15}'.format(
+                self.runNum,self.engine.packetsSentToRoot,self.engine.packetReceivedInRoot,self.engine.olGeneratedToRoot/(self.settings.numMotes-1),self.engine.thReceivedInRoot/(self.settings.numMotes-1), self.engine.dropByCollision, self.engine.totalTx, self.engine.totalRx, self.engine.dropByPropagation, self.engine.getMaxJoiningTime()/self.settings.slotframeLength, self.engine.getMaxNodeHasTxCellsTime()/self.settings.slotframeLength,int(self.engine.getMaxNodeSendingTime()/self.settings.slotframeLength), self.engine.getAvgVisibleNeighbors(),self.engine.TRX, self.engine.RDX, self.engine.Locked)
+		  ]
+	output  = '\n'.join(output)
+        
+        with open(self.settings.getOutputFile(),'a') as f:
+            f.write(output)
+
     def _fileWriteTopology(self):
         output  = []
         output += [
@@ -267,23 +393,12 @@ class SimStats(object):
                 ' '.join(['{0}-{1}@{2:.0f}dBm@{3:.3f}'.format(moteA.id,moteB.id,rssi,pdr) for ((moteA,moteB),(rssi,pdr)) in links.items()])
             )
         ]
-	#emunicio
-	output += [
-            '#results runNum={0} totalPacketSent {1} totalPacketReceived {2} totalOLGenerated {3} totalThReceived {4} dropsByCollisions {5} totalTX {6} totalRX {7} dropsPropagation {8} -'.format(
-                self.runNum,self.engine.packetsSentToRoot,self.engine.packetReceivedInRoot,self.engine.olGeneratedToRoot/(self.settings.numMotes-1),self.engine.thReceivedInRoot/(self.settings.numMotes-1), self.engine.dropByCollision, self.engine.totalTx, self.engine.totalRx, self.engine.dropByPropagation 
-            )
-        ]
-
         output += [
             '#aveChargePerCycle runNum={0} {1}'.format(
                 self.runNum,
                 ' '.join(['{0}@{1:.2f}'.format(mote.id,mote.getMoteStats()['chargeConsumed']/self.settings.numCyclesPerRun) for mote in self.engine.motes])
             )
-        ] 
-	
-	output += [ ''
-        ]
-	       
+        ]        
         output  = '\n'.join(output)
         
         with open(self.settings.getOutputFile(),'a') as f:
